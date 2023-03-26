7fcae0d9ff32ce1c0c952485eb94ffc8810cd866
shun198 <shunhiroseluvmri@gmail.com>
2023-03-26 18:45:40 +0900

    👍 add egress rules

--- infra/database.tf
+++ infra/database.tf
@@ -26,6 +26,13 @@ resource "aws_security_group" "rds" {
     to_port   = 5432
   }
 
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   tags = local.common_tags
 }
 
