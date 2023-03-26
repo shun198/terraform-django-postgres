8495634917181940f4291689c91e5fb4c3849497
shun198 <shunhiroseluvmri@gmail.com>
2023-03-26 17:23:02 +0900

    👍 add sg and vpc settings for rds

--- infra/database.tf
+++ infra/database.tf
@@ -1,3 +1,30 @@
 # ------------------------------
 # Database Configuration
 # ------------------------------
+resource "aws_db_subnet_group" "main" {
+  name = "${local.prefix}-main"
+  subnet_ids = [
+    aws_subnet.private_a.id,
+    aws_subnet.private_c.id,
+  ]
+
+
+  tags = merge(
+    local.common_tags,
+    tomap({ "Name" = "${local.prefix}-main" })
+  )
+}
+
+resource "aws_security_group" "rds" {
+  description = "Allow access to RDS"
+  name        = "${local.prefix}-rds-inbound-access"
+  vpc_id      = aws_vpc.main.id
+
+  ingress = {
+    protocol  = "tcp"
+    from_port = 5432
+    to_port   = 5432
+  }
+
+  tags = local.common_tags
+}
