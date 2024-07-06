3d9e805aea862432a2951997c4d66eda3162fb98
shun198 <shunhiroseluvmri@gmail.com>
2024-07-06 13:32:17 +0900

    👍 update main.tf

--- main.tf
+++ main.tf
@@ -4,11 +4,11 @@
 terraform {
   # tfstateファイルを管理するようbackend(s3)を設定
   backend "s3" {
-    bucket         = "terraform-playground-for-cicd-shun198"
-    key            = "terrafrom-playground.tfstate"
+    bucket         = "terrafrom-practice-shun198"
+    key            = "terrafrom-practice.tfstate"
     region         = "ap-northeast-1"
     encrypt        = true
-    dynamodb_table = "terraform-playground-tf-state-lock"
+    dynamodb_table = "terrafrom-practice-tf-state-lock"
   }
   # プロバイダを設定
   required_providers {
@@ -35,3 +35,62 @@ provider "aws" {
 # ------------------------------
 # 現在のAWS Regionの取得方法
 data "aws_region" "current" {}
+
+module "certificates" {
+  providers = {
+    aws.default       = aws.default
+    aws.global_region = aws.global_region
+  }
+  source                    = "./modules/certificates"
+  project_name              = var.project_name
+  infra_environment         = var.infra_environment
+  domain                    = var.domain
+  zone_id                   = var.zone_id
+  cloudfront_hosted_zone_id = data.aws_route53_zone.amplify.zone_id
+}
+
+locals {
+  az_one_name              = split("-", var.availability_zone_one)[2]
+  az_two_name              = split("-", var.availability_zone_two)[2]
+  alb_auth_header_password = base64encode(random_password.alb_password.result)
+  amplify_auth             = base64encode("${var.amplify_username}:${var.amplify_password}")
+  is_prd                   = var.infra_environment == "prd"
+  sns_alert_name           = "shun198-alert"
+  sns_warning_name         = "shun198-warning"
+  sns_approve_name         = "shun198-approve"
+  sns_security_name        = "shun198-security"
+  sns_health_name          = "shun198-health"
+  sns_cicd_name            = "shun198-cicd-pipeline"
+  sns_config_name          = "shun198-config"
+}
+
+module "amplify" {
+  source                   = "./modules/amplify"
+  project_name             = var.project_name
+  domain                   = var.domain
+  infra_environment        = var.infra_environment
+  amplify_username         = var.amplify_username
+  amplify_password         = var.amplify_password
+  github_repository_front  = var.github_repository_front
+  github_branch_front      = var.github_branch_front
+  is_prd                   = local.is_prd
+  alb_auth_header_name     = var.alb_auth_header_name
+  alb_auth_header_password = local.alb_auth_header_password
+  cicd_topic_arn           = module.cicd.cicd_topic_arn
+  github_access_token      = var.github_access_token
+}
+
+module "cloudfront" {
+  source                     = "./modules/cloudfront"
+  project_name               = var.project_name
+  domain                     = var.domain
+  infra_environment          = var.infra_environment
+  is_prd                     = local.is_prd
+  github_branch_front        = var.github_branch_front
+  cache_policy_id            = var.cache_policy_id
+  response_headers_policy_id = var.response_headers_policy_id
+  amplify_auth               = local.amplify_auth
+  amplify_default_domain     = module.amplify.amplify_default_domain
+  cloudfront_certificate_arn = module.certificates.cloudfront_certificate_arn
+  cloudfront_hosted_zone_id  = data.aws_route53_zone.amplify.zone_id
+}
--- variables.tf
+++ variables.tf
@@ -1,3 +1,119 @@
 # ------------------------------
 # Variables
 # ------------------------------
+variable "project_name" {
+  type        = string
+  description = "Name of the project you are managing."
+  nullable    = false
+}
+
+variable "domain" {
+  description = "The domain to use with the project."
+  type        = string
+  nullable    = false
+}
+
+variable "infra_environment" {
+  description = "The environment used for creating the infrastructure."
+  type        = string
+  nullable    = false
+  validation {
+    condition     = contains(["dev", "stg", "prd"], var.infra_environment)
+    error_message = "infra_environment must be one of the following: [dev, stg, prd]."
+  }
+}
+
+# ------------------------------
+# SES
+# ------------------------------
+variable "zone_id" {
+  type        = string
+  description = "Route53 parent zone ID. This will create Route53 DNS records used for verification."
+  default     = ""
+  nullable    = false
+}
+
+# ------------------------------
+# VPC
+# ------------------------------
+variable "availability_zone_one" {
+  type        = string
+  description = "Availability Zone to use for deployment(1)."
+  nullable    = false
+}
+
+variable "availability_zone_two" {
+  type        = string
+  description = "Availability Zone to use for deployment(2)."
+  nullable    = false
+}
+
+# ------------------------------
+# ALB
+# ------------------------------
+variable "alb_auth_header_name" {
+  type        = string
+  description = "Auth header name for the ALB to restrict access to our Amplify application."
+  nullable    = false
+}
+
+# ------------------------------
+# Frontend
+# ------------------------------
+variable "cache_policy_id" {
+  type        = string
+  description = "Cache policy to use for cloudfront. https://docs.aws.amazon.com/ja_jp/AmazonCloudFront/latest/DeveloperGuide/using-managed-cache-policies.html"
+  nullable    = false
+}
+
+variable "response_headers_policy_id" {
+  type        = string
+  description = "Response headers policy to use for cloudfront. https://docs.aws.amazon.com/ja_jp/AmazonCloudFront/latest/DeveloperGuide/using-managed-response-headers-policies.html"
+  nullable    = false
+}
+
+variable "amplify_username" {
+  type        = string
+  description = "Username for amplify authorization."
+  nullable    = false
+}
+
+variable "amplify_password" {
+  type        = string
+  description = "Password for amplify authorization."
+  nullable    = false
+}
+
+variable "github_access_token" {
+  type        = string
+  description = "Github access token used to connect to the repository."
+  nullable    = false
+}
+
+variable "github_repository_front" {
+  type        = string
+  description = "Full path of the github repository for the frontend."
+  nullable    = false
+}
+
+variable "github_branch_front" {
+  type        = string
+  description = "Which branch to use for deployment"
+  nullable    = false
+}
+
+# ------------------------------
+# Backend
+# ------------------------------
+variable "github_repository_back" {
+  type        = string
+  description = "Full path of the github repository for the backend."
+  nullable    = false
+}
+
+variable "github_branch_back" {
+  type        = string
+  description = "Which branch to use for deployment"
+  nullable    = false
+}
+
